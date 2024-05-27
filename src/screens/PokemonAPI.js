bc5657d2f1d1b9ac9b9f2725c5d00c9d7fc7cfd0
Feraleck2005 <20190131@ricaldone.edu.sv>
2024-05-27 09:39:18 -0600

    first commit

--- src/screens/PokemonAPI.js
+++ src/screens/PokemonAPI.js
@@ -12,9 +12,11 @@ export default function PokemonAPI() {
 
   const fetchPokemons = async () => {
     try {
+      console.log('Fetching Pokemon lista');
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}`);
       const data = await response.json();
-      console.log('Pokemon list response:', data.results);
+      console.log('Pokemon lista :', data.results);
+
       const detailedData = await Promise.all(
         data.results.map(async (pokemon) => {
           const pokemonDetailsResponse = await fetch(pokemon.url);
@@ -28,11 +30,12 @@ export default function PokemonAPI() {
           };
         })
       );
-      console.log('Detailed Pokemon data:', detailedData);
+
+      console.log('Detalle Pokemon data:', detailedData);
       setPokemons(detailedData);
     } catch (error) {
       console.error('Error fetching Pokemons:', error);
-      Alert.alert('Error', 'Error fetching Pokemons. Please try again.');
+      Alert.alert('Error', 'Error fetching Pokemons. Vuelva a intentar.');
     }
   };
 
@@ -40,11 +43,11 @@ export default function PokemonAPI() {
     try {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
       const data = await response.json();
-      const flavorText = data.flavor_text_entries.find(entry => entry.language.name === 'en');
-      return flavorText ? flavorText.flavor_text : 'No description available.';
+      const flavorText = data.flavor_text_entries.find(entry => entry.language.name === 'es');
+      return flavorText ? flavorText.flavor_text : 'No descripcion disponible.';
     } catch (error) {
-      console.error('Error fetching Pokemon description:', error);
-      return 'No description available.';
+      console.error('Error Pokemon descripcion:', error);
+      return 'No descripcion disponible.';
     }
   };
 
@@ -56,7 +59,7 @@ export default function PokemonAPI() {
     <TouchableOpacity style={styles.card}>
       <Image source={{ uri: item.sprite }} style={styles.image} />
       <Text style={styles.name}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</Text>
-      <Text style={styles.details}>Abilities: {item.abilities}</Text>
+      <Text style={styles.details}>Habilidad: {item.abilities}</Text>
       <Text style={styles.details}>{item.description}</Text>
     </TouchableOpacity>
   );
@@ -77,13 +80,17 @@ export default function PokemonAPI() {
         onChangeText={(text) => setSearch(text)}
         value={search}
       />
-      <FlatList
-        data={filteredPokemons}
-        renderItem={renderPokemonCard}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={2}
-        contentContainerStyle={styles.list}
-      />
+      {filteredPokemons.length > 0 ? (
+        <FlatList
+          data={filteredPokemons}
+          renderItem={renderPokemonCard}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={1}
+          contentContainerStyle={styles.list}
+        />
+      ) : (
+        <Text style={styles.noResults}>No Pokemons found</Text>
+      )}
     </View>
   );
 }
@@ -119,7 +126,6 @@ const styles = StyleSheet.create({
     margin: 5,
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
     elevation: 3,
   },
   image: {
@@ -136,4 +142,9 @@ const styles = StyleSheet.create({
     marginTop: 5,
     textAlign: 'center',
   },
-})
+  noResults: {
+    fontSize: 16,
+    marginTop: 20,
+    color: 'red',
+  },
+});
